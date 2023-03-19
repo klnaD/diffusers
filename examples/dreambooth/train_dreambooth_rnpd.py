7ed73451bd7515d15abaa4bab1786c05ee05a7e7
Ben <110583491+TheLastBen@users.noreply.github.com>
2023-03-19 21:33:57 +0100

    Update train_dreambooth_rnpd.py

--- examples/dreambooth/train_dreambooth_rnpd.py
+++ examples/dreambooth/train_dreambooth_rnpd.py
@@ -355,8 +355,8 @@ class DreamBoothDataset(Dataset):
 
         self.image_transforms = transforms.Compose(
             [
-                transforms.Resize(size, interpolation=transforms.InterpolationMode.BILINEAR),
-                transforms.CenterCrop(size) if center_crop else transforms.RandomCrop(size),
+                #transforms.Resize(size, interpolation=transforms.InterpolationMode.BILINEAR),
+                #transforms.CenterCrop(size) if center_crop else transforms.RandomCrop(size),
                 transforms.ToTensor(),
                 transforms.Normalize([0.5], [0.5]),
             ]
